6189d0732523cfaad6498c527cd782659aeae832
Juan.J.Diaz BairesDev <juanjo.fde@gmail.com>
2021-10-19 13:06:36 -0500

    Formats PDFreport

--- App-2-Flatmates-Bill/main.py
+++ App-2-Flatmates-Bill/main.py
@@ -39,17 +39,28 @@ class PdfReport:
         pdf = FPDF(orientation='P', unit='pt', format='A4')
         pdf.add_page()
 
+        # Add icon
+        pdf.image("files\house.png", w=30, h=30)
+
         # Insert title
         pdf.set_font(family='Times', size=24, style='B')
         pdf.cell(w=0, h=80, txt='Flatmates Bill', border=1, align='C', ln=1)
 
         # Insert Period label and value
-        pdf.cell(w=100, h=40, txt='Period:', border=1)
-        pdf.cell(w=150, h=40, txt= bill.period, border=1, ln=1)
+        pdf.set_font(family="Times", size=14, style='B')
+        pdf.cell(w=100, h=40, txt='Period:', border=0)
+        pdf.cell(w=150, h=40, txt= bill.period, border=0, ln=1)
 
         # Insert name and due amount of 1st flatmate
-        pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
-        pdf.cell(w=150, h=40, txt=str(flatmate1.pays(bill=bill, flatmate2=flatmate2)), border=1, ln=1)
+        pdf.set_font(family="Times", size=12)
+        flatmate1_pay = str(round(flatmate1.pays(bill=bill, flatmate2=flatmate2), 2))
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate1_pay, border=0, ln=1)
+
+        # Insert name and due amount of 2nd flatmate
+        flatmate2_pay = str(round(flatmate2.pays(bill=bill, flatmate2=flatmate2), 2))
+        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate2_pay, border=0, ln=1)
 
         # Saving file
         pdf.output(self.filename)
