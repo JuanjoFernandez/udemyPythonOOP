feca13ffd3fba8ae2cc87ffd9dac68fcbed58614
Juan.J.Diaz BairesDev <juanjo.fde@gmail.com>
2021-10-20 12:22:30 -0500

    Creates CLI interface

--- App-2-Flatmates-Bill/main.py
+++ App-2-Flatmates-Bill/main.py
@@ -1,5 +1,7 @@
 # Libraries and dependencies
 from fpdf import FPDF
+import webbrowser
+
 
 class Bill:
     """
@@ -64,9 +66,14 @@ class PdfReport:
 
         # Saving file
         pdf.output(self.filename)
+        webbrowser.open(self.filename)
+
+a = float(input('Hey user, enter the bill amount: '))
+print ('A=', a)
+print (type(a))
 
 # Output
-the_bill = Bill(amount=120, period="April 2021")
+the_bill = Bill(amount=a, period="April 2021")
 john = Flatmate(name="John", days_in_house=20)
 marry = Flatmate(name="Marry", days_in_house=25)
 
