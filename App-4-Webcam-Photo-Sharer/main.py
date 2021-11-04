ba7b772c2efd0e4f95a2da90744de90c6ffb386f
Juan.J.Diaz BairesDev <juanjo.fde@gmail.com>
2021-11-04 11:24:18 -0600

    Adds webcam capabilities

--- App-4-Webcam-Photo-Sharer/main.py
+++ App-4-Webcam-Photo-Sharer/main.py
@@ -8,10 +8,16 @@ Builder.load_file('frontend.kv')
 
 class CameraScreen(Screen):
     def start(self):
-        pass
+        print("start")
+        self.ids.camera.play = True
+        self.ids.camera_button.text = "Stop Camera"
+        self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
-        pass
+        print('stop')
+        self.ids.camera.play = False
+        self.ids.camera_button.text = "Start Camera"
+        self.ids.camera.texture = None
 
     def capture(self):
         pass
