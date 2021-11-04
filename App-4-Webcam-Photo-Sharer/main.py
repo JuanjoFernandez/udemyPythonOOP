6bd451dc785eec764be37d342e9374fba1fa35e7
Juan.J.Diaz BairesDev <juanjo.fde@gmail.com>
2021-11-04 11:32:14 -0600

    Adds capture function

--- App-4-Webcam-Photo-Sharer/main.py
+++ App-4-Webcam-Photo-Sharer/main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+import time
 
 from filesharer import FileSharer
 
@@ -20,7 +21,9 @@ class CameraScreen(Screen):
         self.ids.camera.texture = None
 
     def capture(self):
-        pass
+        current_time = time.strftime('%Y%m%d-%H%M%S')
+        filepath = f'files/{current_time}.png'
+        self.ids.camera.export_to_png(filepath)
 
 
 class ImageScreen(Screen):
