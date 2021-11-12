9efefc91cf3d62680e865a4b27b6fec99d811c89
Juan.J.Diaz BairesDev <juanjo.fde@gmail.com>
2021-11-12 09:00:02 -0600

    Adds scrapped temperature

--- App-6-Project-Calorie-Webapp/calorie.py
+++ App-6-Project-Calorie-Webapp/calorie.py
@@ -1,7 +1,16 @@
-class Calorie:
-    def __init__(self, country, city):
-        self.country = country
-        self.city = city
+import requests
+from selectorlib import Extractor
+from temperature import Temperature
 
-    def get(self):
+
+"""Stores input data and calculates daily calorie intake"""
+
+class Calorie:
+    def __init__(self, weight, height, age, temperature):
+        self.weight = weight
+        self.height = height
+        self.age = age
+        self.temperature = temperature
+
+    def calculate(self):
         pass
--- App-6-Project-Calorie-Webapp/temperature.py
+++ App-6-Project-Calorie-Webapp/temperature.py
@@ -1,9 +1,19 @@
-class Calorie:
-    def __init__(self, weight, height, age, temperature):
-        self.weight = weight
-        self.height = height
-        self.age = age
-        self.temperature = temperature
+import requests
+from selectorlib import Extractor
 
-    def calculate(self):
-        pass
+
+class Temperature:
+    """Scraps city temperature from timeanddate.com"""
+    def __init__(self, country, city):
+        self.country = country
+        self.city = city
+
+    def get(self):
+        url = f'https://www.timeanddate.com/weather/{self.country}/{self.city}'
+        request = requests.get(url)
+        text = request.text
+        extractor = Extractor.from_yaml_file('temperature.yaml')
+        raw = extractor.extract(text)
+        temperature = float(raw['temp'].replace('\xa0°F', ""))
+
+        return temperature
