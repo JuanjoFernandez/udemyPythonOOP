63294bb30fa7fe793d3e2274f80db5e29ef96b20
Juan.J.Diaz BairesDev <juanjo.fde@gmail.com>
2021-11-16 14:22:37 -0600

    Stores titles and url's for news

--- App-7-Automated-Emails/main.py
+++ App-7-Automated-Emails/main.py
@@ -1,3 +1,8 @@
+from config import api_key
+import requests
+import pandas as pd
+from pprint import pprint
+
 class NewsFeed:
 
     def __init__(self, data):
@@ -5,3 +10,22 @@ class NewsFeed:
     
     def get(self):
         pass
+
+people = pd.read_excel('people.xlsx', usecols="A:D")
+people.dropna(inplace=True)
+
+# API call
+url = 'https://newsapi.org/v2/everything?' \
+      'qInTitle=apple&' \
+      'from=2021-11-15&to=2021-11-15&sortBy=popularity&'
+request = requests.get(url + 'apiKey=' + api_key)
+response = request.json()
+articles = response['articles']
+news = []
+for article in articles:
+    art = (article['title'], article['url'])
+    news.append(art)
+for _ in news:
+    print(_)
+
+
