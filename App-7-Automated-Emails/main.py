852a434a92785c3b070aa23afb1515570ae8c4af
Juan.J.Diaz BairesDev <juanjo.fde@gmail.com>
2021-11-18 12:26:39 -0600

    Implements NewsFeed class

--- App-7-Automated-Emails/main.py
+++ App-7-Automated-Emails/main.py
@@ -4,28 +4,37 @@ import pandas as pd
 from pprint import pprint
 
 class NewsFeed:
+    """Representing multiple news and links as a single string"""
 
-    def __init__(self, data):
-        self.data = data
+    base_url = 'https://newsapi.org/v2/everything?'
+    def __init__(self, interest, from_date, to_date, language):
+        self.interest = interest
+        self.from_date = from_date
+        self.to_date = to_date
+        self.language = language
     
     def get(self):
-        pass
+        # API call
+        url = f'https://newsapi.org/v2/everything?' \
+            f'qInTitle={self.interest}&' \
+            f'from={self.from_date}&to={self.to_date}&' \
+            f'language={self.language}&'
+        request = requests.get(url + 'apiKey=' + api_key)
+        response = request.json()
+        articles = response['articles']
+
+        email_body = ''
+
+        for article in articles:
+            email_body = email_body +  article['title'] + '\n' + article['url'] + '\n\n'
+
+        print(email_body)
 
 people = pd.read_excel('people.xlsx', usecols="A:D")
 people.dropna(inplace=True)
 
-# API call
-url = 'https://newsapi.org/v2/everything?' \
-      'qInTitle=apple&' \
-      'from=2021-11-15&to=2021-11-15&sortBy=popularity&'
-request = requests.get(url + 'apiKey=' + api_key)
-response = request.json()
-articles = response['articles']
-news = []
-for article in articles:
-    art = (article['title'], article['url'])
-    news.append(art)
-for _ in news:
-    print(_)
+news_feed = NewsFeed('nasa', '2021-11-12', '2021-11-12', 'en').get()
+
+
 
 
