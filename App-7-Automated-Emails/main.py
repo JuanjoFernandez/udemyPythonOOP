c2cc14ee3f12c1d3c2ec178bd7c6dc8d086c2658
Juan.J.Diaz BairesDev <juanjo.fde@gmail.com>
2021-11-18 15:03:07 -0600

    Adds daily functionality

--- App-7-Automated-Emails/main.py
+++ App-7-Automated-Emails/main.py
@@ -3,19 +3,25 @@ from config import e_mail_user, e_mail_password
 import pandas as pd
 from news import NewsFeed
 import datetime as dt
+import time
 
-people = pd.read_excel('people.xlsx', usecols="A:D")
-people.dropna(inplace=True)
+while True:
+    if dt.datetime.now().hour == 15 and dt.datetime.now().minute == 1:
 
-today = dt.datetime.now().strftime('%Y-%m-%d')
-yesterday = (dt.datetime.now() - dt.timedelta(days=1)).strftime('%Y-%m-%d')
+        people = pd.read_excel('people.xlsx', usecols="A:D")
+        people.dropna(inplace=True)
 
-for index, row in people.iterrows():
-    news_feed = NewsFeed(interest=row['interest'],
-                         from_date=yesterday, 
-                         to_date=today)
+        today = dt.datetime.now().strftime('%Y-%m-%d')
+        yesterday = (dt.datetime.now() - dt.timedelta(days=1)).strftime('%Y-%m-%d')
+
+        for index, row in people.iterrows():
+            news_feed = NewsFeed(interest=row['interest'],
+                                from_date=yesterday, 
+                                to_date=today)
+            
+            email = yagmail.SMTP(user=e_mail_user, password = e_mail_password)
+            email.send(to=row['email'], 
+                    subject=f"Your {row['interest']} news for today!",
+                    contents=f"Hi {row['name']}\n See what's on about {row['interest']} today.\n{news_feed.get()}JJ",)
     
-    email = yagmail.SMTP(user=e_mail_user, password = e_mail_password)
-    email.send(to=row['email'], 
-            subject=f"Your {row['interest']} news for today!",
-            contents=f"Hi {row['name']}\n See what's on about {row['interest']} today.\n{news_feed.get()}JJ",)
+    time.sleep(60)
