8e3d8038c5eb812915508f6d226caf58e8302035
Juan.J.Diaz BairesDev <juanjo.fde@gmail.com>
2021-11-23 10:49:34 -0600

    Adds grid style

--- App-8-Instant-Dictionary-Webapp/examples/main2.py
+++ App-8-Instant-Dictionary-Webapp/examples/main2.py
@@ -5,12 +5,20 @@ from pandas.core.dtypes.common import classes
 def home():
     wp = jp.WebPage()
     div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
-    jp.Input(a=div, placeholder="Enter first value", classes="form-input")
-    jp.Input(a=div, placeholder="Enter second value", classes="form-input")
-    jp.Div(a=div, text="Result goes here...", classes="text-gray-600")
-    jp.Button(a=div, text="Calculate",
+
+    div1 = jp.Div(a=div, classes="grid grid-cols-3 gap-4")
+    jp.Input(a=div1, placeholder="Enter first value", classes="form-input")
+    jp.Input(a=div1, placeholder="Enter second value", classes="form-input")
+    jp.Div(a=div1, text="Result goes here...", classes="text-gray-600")
+    jp.Div(a=div1, text="One div", classes="text-gray-600")
+    jp.Div(a=div1, text="Another div", classes="text-gray-600")
+    
+    div2 = jp.Div(a=div, classes="grid grid-cols-2 gap-4")
+    jp.Button(a=div2, text="Calculate",
               classes="border border-blue-500 m-2 py-1 px-4 rounded "
                       "text-blue-600 hover:bg-blue-900 hover:text-white")
+
+    jp.Div(a=div2, text="I am a cool interactive div!")
     return wp
 
 jp.justpy()
