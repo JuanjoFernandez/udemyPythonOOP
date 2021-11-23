3f9156640962b253f1827a63c7aa98bd1b812116
Juan.J.Diaz BairesDev <juanjo.fde@gmail.com>
2021-11-23 10:43:20 -0600

    Adds styling

--- App-8-Instant-Dictionary-Webapp/examples/main2.py
+++ App-8-Instant-Dictionary-Webapp/examples/main2.py
@@ -1,19 +1,16 @@
 import justpy as jp
+from pandas.core.dtypes.common import classes
 
 @jp.SetRoute('/home')
 def home():
     wp = jp.WebPage()
-    jp.Div(a=wp, text="Hello World!", 
-           classes="text-green-200 bg-yellow-500 "
-                   "font-serif text-lg")
-    jp.Div(a=wp, text="Hello again!")
-    return wp
-
-@jp.SetRoute('/about')
-def about():
-    wp = jp.WebPage()
-    jp.Div(a=wp, text="Hi World!")
-    jp.Div(a=wp, text="Hey again!")
+    div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+    jp.Input(a=div, placeholder="Enter first value", classes="form-input")
+    jp.Input(a=div, placeholder="Enter second value", classes="form-input")
+    jp.Div(a=div, text="Result goes here...", classes="text-gray-600")
+    jp.Button(a=div, text="Calculate",
+              classes="border border-blue-500 m-2 py-1 px-4 rounded "
+                      "text-blue-600 hover:bg-blue-900 hover:text-white")
     return wp
 
 jp.justpy()
