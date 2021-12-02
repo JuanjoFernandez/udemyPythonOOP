fa76ed86aaa42ccd05466ce6b860d74886d37c32
Juan.J.Diaz BairesDev <juanjo.fde@gmail.com>
2021-12-02 10:53:39 -0600

    Adds button functionality

--- App-8-Instant-Dictionary-Webapp/webapp/dictionary.py
+++ App-8-Instant-Dictionary-Webapp/webapp/dictionary.py
@@ -10,12 +10,19 @@ class Dictionary:
         div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
         jp.Div(a=div, text="Instant English dictionary", classes="text-4xl m-2")
         jp.Div(a=div, text="Get the definition of any English word instantly as you type", classes="text-lg")
-
+        input_div=jp.Div(a=div, classes="grid grid cols-2")
         jp.Input(a=div, placeholder="Type in a word here...",
                  classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 "
                          "focus:outline-none focus:border-purple-500 focus:bg-white"
                          "py-2 px-4")
-        jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
+        
+        output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
+        jp.Button(a=input_div, text="Get definition", classes="border-2 text-gray-500", click=cls.get_definition, outputdiv=output_div)
+        
         
         return wp
     
+    @staticmethod
+    def get_definition(widget, msg):
+        widget.outputdiv.text = "Something was displayed!"
+        
