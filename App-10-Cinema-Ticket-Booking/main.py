0fcbe99a15a41cfebadc677f66267396d990ff2d
Juan.J.Diaz BairesDev <juanjo.fde@gmail.com>
2022-01-04 16:37:39 -0600

    Adds queries

--- App-10-Cinema-Ticket-Booking/main.py
+++ App-10-Cinema-Ticket-Booking/main.py
@@ -1,6 +1,8 @@
 import random
+from sqlite3.dbapi2 import connect
 import string
 import sqlite3
+from fpdf import FPDF
 
 class User:
     def __init__(self, name) -> None:
@@ -15,18 +17,33 @@ class User:
 class Seat:
     database = 'cinema.db'
     
-    def __init__(self) -> None:
+    def __init__(self, seat_id) -> None:
         self.seat_id = seat_id
 
         pass
 
     def get_price(self):
-
-        return
+        connection = sqlite3.connect('cinema.db')
+        cursor = connection.cursor()
+        query = f"SELECT price FROM Seat WHERE seat_id == '{self.seat_id}'"
+        taken = cursor.execute(query)
+        records = taken.fetchall()
+        connection.close()
+        price = records[0][0] 
+        return price
 
     def is_free(self):
-
-        return
+        connection = sqlite3.connect('cinema.db')
+        cursor = connection.cursor()
+        query = f"SELECT taken FROM Seat WHERE seat_id == '{self.seat_id}'"
+        taken = cursor.execute(query)
+        records = taken.fetchall()
+        connection.close()
+        if (records[0][0]) == 0:
+            return True
+        else:
+            print("That seat is not available")
+            return False
 
     def occupy(self):
 
@@ -34,7 +51,6 @@ class Seat:
 
 
 class Card:
-    database = 'banking.db'
     def __init__(self, card_type, card_number, card_cvc, holder) -> None:
         self.type = card_type
         self.card_number = card_number
@@ -43,8 +59,24 @@ class Card:
         pass
 
     def validate(self, price):
-
-        return
+        connection = sqlite3.connect('banking.db')
+        cursor = connection.cursor()
+        query = f"SELECT number FROM Card WHERE number == '{self.card_number}';"
+        exists = cursor.execute(query)
+        record = exists.fetchall()
+        if record:
+            balance = record[0][4]
+            new_balance = balance - int(price)
+            balance = new_balance
+            query = f"UPDATE Card SET balance={new_balance};"
+            
+        else: 
+            query = f"INSERT INTO Card VALUES ('{self.type}', '{self.card_number}', '{self.card_cvc}', '{self.holder}', '{1000}');"
+            cursor.execute(query)
+            balance = 1000
+        
+        connection.close()
+        return balance
 
 
 class Ticket:
@@ -59,6 +91,51 @@ class Ticket:
 
         return
 
-name = input ("Welcome to the cinema booking app. What's your name?:")
+# Get client name
+
+name = 'John'
+# name = input("Welcome to the cinema booking app. What's your name?:")
+
+# Get seat_id and validate
+seat_valid = False
+while not seat_valid:
+    
+    # seat = Seat(input("Please choose your seat (A1, A2, A3, B1, B2, B3):"))
+    seat = Seat('A1')
+
+    seat_valid = seat.is_free()
+
+# Get all the credit card info
+valid_card = False
+while valid_card == False:
+    card_number = '1234123412341234'
+    card_cvc = '111'
+    card_holder = 'JOHN S. DOE'
+    price = seat.get_price()
+    # card_number = input(f'Your seat {seat.seat_id} is available with a price of ${price} please enter your credit card number:')
+    # card_cvc = input('Please enter the csv number:')
+    # card_holder = input('Please enter the card holder name:')
+
+    # Validate the credit card 
+    if len(card_number) == 16 and card_number.isnumeric() and \
+       len(card_cvc) == 3 and card_cvc.isnumeric():
+       valid_card = True
+    else:
+        print ("Your credit card information is invalid")
+
+# For mockup purposes, MasterCard and Visa numbers are actually different
+if int(card_number[0:3]) <= 50:
+    card_type = 'MasterCard'
+else:
+    card_type = 'Visa'
+
+credit_card = Card(card_type, card_number, card_cvc, card_holder)
+charge = credit_card.validate(price)
+
+print ("Transaction succesfull, generating ticket, enjoy your movie")
+
+# Generating ticket
+ticket = Ticket(name, price, seat.seat_id)
+ticket.to_pdf("ticket.pdf")
 
 
