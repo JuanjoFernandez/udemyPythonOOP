98a8fb672ba0ffe0c65a772a13d7e069b802016f
Juan.J.Diaz BairesDev <juanjo.fde@gmail.com>
2022-01-07 13:11:00 -0600

    Adds documentation

--- App-X_WeatherLibrary/sunnyday.py
+++ App-X_WeatherLibrary/sunnyday.py
@@ -3,7 +3,26 @@ import requests
 from pprint import pprint
 
 class Weather:
+    """Creates a weather object gatting an apikey as input and either a city name or lat and lon coordinates.
 
+    Package use example:
+
+    # Create a weather object using a city name:
+    # You need to obtain an API key from: https://openweathermap.org
+    # After registering and obtaining your API key it can take up to two hours to be active
+
+    >>> weather1 = Weather(apikey = "your API key", city = "Madrid")
+
+    # Using latitude and longitude coordinates
+    >>> weahter2 = Weather(apikey = "your API key", lat = 41.1, lon = -4.1)
+
+    # Get complete weather data for the next 12 hours:
+    >>> weather1.next_12h()
+
+    # Simplified data for the next 12 hours:
+    >>> weather1.next_12h_simplified()
+    
+    """
     def __init__(self, apikey, city=None, lat=None, lon=None):
         
         if city:
@@ -21,9 +40,14 @@ class Weather:
             raise ValueError(self.data['message'])
     
     def next_12h(self):
+        """ Returns 3-hour data for the next 12 hours as a dict.
+        """
         return self.data['list'][:4]
 
     def next_12h_simplified(self):
+        """ Returns date, temperature, and sky condition every 3 hours for the next
+            12 hours as a tuple of tuples.
+        """
         simple_data = []
         for time in self.data['list'][:4]:
             simple_data.append((time['dt_txt'], time['main']['temp'], time['weather'][0]['description']))
